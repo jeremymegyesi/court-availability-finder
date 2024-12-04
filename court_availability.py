ad1425ec34bfd204b6f5980c77dea3a1c095cb7e
Jeremy Megyesi <jeremy.megyesi@gmail.com>
2024-12-03 16:23:56 -0800

    Running IO bound requests concurrently using asyncio, much faster!

--- court_availability.py
+++ court_availability.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 from datetime import datetime, timezone, timedelta
 import json
 import re
+import asyncio
+import aiohttp
 import date_utils
 
 
@@ -19,65 +21,67 @@ class color:
    END = '\033[0m'
 
 # compares a given spot with inputted availability
-def spotMatchesAvailability(spot, avail):
+def spot_matches_avail(spot, avail):
     for availWindow in avail:
         if (date_utils.is_day_between(spot['day'], availWindow['dayStart'], availWindow['dayEnd']) and \
             not spot['timeStart'] < availWindow['timeStart'] and \
             not spot['timeEnd'] > availWindow['timeEnd']):
             return True
+    return False
 
+def get_user_input():
+    # prompt user for availability
+    duration = int(input(color.BOLD + 'Enter duration (mins): ' + color.END))
+    rawAvail = input(color.BOLD + 'Enter availability...' + color.END + '\n\
+    (ex. "tue 06-22:30,thu-fri 08:30-12")\n\
+    ("any xx-xx" - any day of week. blank for all availability):\n')
 
-# prompt user for availability
-duration = int(input(color.BOLD + 'Enter duration (mins): ' + color.END))
-rawAvail = input(color.BOLD + 'Enter availability...' + color.END + '\n\
-(ex. "tue 06-22:30,thu-fri 08:30-12")\n\
-("any xx-xx" - any day of week. blank for all availability):\n')
+    # TODO: validate user input
 
-# validate user input
+    # parse user input
+    avail = rawAvail.split(',')
+    for i, window in enumerate(avail):
+        window = window.split(' ')
+        day = window[0].split('-')
+        t = window[1].split('-')
+        avail[i] = {
+            'dayStart': day[0],
+            'dayEnd': day[1] if len(day) == 2 else day[0],
+            'timeStart': date_utils.extract_time(t[0]),
+            'timeEnd': date_utils.extract_time(t[1])
+        }
 
-# parse user input
-avail = rawAvail.split(',')
-for i, window in enumerate(avail):
-   window = window.split(' ')
-   day = window[0].split('-')
-   t = window[1].split('-')
-   avail[i] = {
-      'dayStart': day[0],
-      'dayEnd': day[1] if len(day) == 2 else day[0],
-      'timeStart': date_utils.extract_time(t[0]),
-      'timeEnd': date_utils.extract_time(t[1])
+    return {
+        'duration': duration,
+        'availability': avail
     }
 
-# load facility data
-with open('facility_data.json', 'r') as file:
-    facility_data = json.load(file)
+async def request_data(facility_data, location_data, duration):
+    async with aiohttp.ClientSession() as session:
+        # need get req to set session cookies and obtain rv token before availability post req
+        async with session.get(f"{facility_data['baseURL']}/Facility?facilityId={facility_data['facilityId']}") as get_req:
+            content = await get_req.read()
+            soup = BeautifulSoup(content, 'lxml')
+            form_token = soup.find('input', attrs={'name': '__RequestVerificationToken'})['value']
 
-# get availability data
-session = HTMLSession()
-def request_data(session, facility_data):
-    gr = session.get(f'{facility_data['baseURL']}/Facility?facilityId={facility_data['facilityId']}')
-    f = open("extracted-page.html", "w", encoding="utf-8")
-    f.write(gr.content.decode('utf-8'))
-    f.close()
+            post_data = [
+                ('facilityId', facility_data['facilityId']),
+                ('daysCount', facility_data['daysCount']),
+                ('serviceId', facility_data['serviceId']),
+                ('date', datetime.now(timezone.utc).isoformat()),
+                ('duration', duration),
+                ('__RequestVerificationToken', form_token),
+            ]
+            for durationId in location_data['durationIds']:
+                post_data.append(('durationIds[]', durationId))
 
-    soup = BeautifulSoup(gr.content, "lxml")
-    formToken = soup.find('input', attrs={'name': '__RequestVerificationToken'})['value']
-    post_data = [
-        ('facilityId', facility_data['facilityId']),
-        ('daysCount', facility_data['daysCount']),
-        ('serviceId', facility_data['serviceId']),
-        ('date', datetime.now(timezone.utc).isoformat()),
-        ('duration', duration),
-        ('__RequestVerificationToken', formToken),
-    ]
-    for durationId in location_data['durationIds']:
-        post_data.append(('durationIds[]', durationId))
+            r = await session.post(
+                f"{facility_data['baseURL']}/FacilityAvailability",
+                data=post_data,
+            )
+            return await r.read()
 
-    r = session.post(f'{facility_data['baseURL']}/FacilityAvailability', data=post_data)
-    # print('Request response: ', r.status_code, r.reason)
-    return r
-
-def getMatches(booking_avail, personal_avail, labels):
+def get_matches(booking_avail, personal_avail, labels, duration):
     avail_days = booking_avail['availabilities']
 
     matches = []
@@ -90,6 +94,9 @@ def getMatches(booking_avail, personal_avail, labels):
         date = datetime.fromtimestamp(timestamp_sec, tz=timezone.utc)
         # Get the day of the week
         day_of_week = date.strftime("%a").lower()
+        # Skip non-matching days
+        if (not filter(lambda x: date_utils.is_day_between(day_of_week, x.dayStart, x.dayEnd), personal_avail)):
+            next
         for bookingGroup in day['BookingGroups']:
             for availableSpot in bookingGroup['AvailableSpots']:
                 # calculate end time
@@ -105,37 +112,53 @@ def getMatches(booking_avail, personal_avail, labels):
                     'timeStart': start_time,
                     'timeEnd': end_time
                 }
-                if (spotMatchesAvailability(formattedSpot, personal_avail)):
+                if (spot_matches_avail(formattedSpot, personal_avail)):
                     matches.append(formattedSpot)
     return matches
 
-all_matches = []
+async def get_facility_matches(location, location_data, facility, duration, avail):
+    refined_data = {
+        'baseURL': location_data['baseURL'],
+        'daysCount': location_data['daysCount'],
+        'serviceId': location_data['serviceId']
+    }
+    refined_data.update(facility)
+    r = await request_data(refined_data, location_data, duration)
+    facility_labels = {
+        'location': location,
+        'facility': facility['name']
+    }
+    return get_matches(json.loads(r.decode('utf-8')), avail, facility_labels, duration)
 
-# iterate through facilities to check their availabilities
-for location in facility_data:
-    location_data = facility_data[location]
-    for facility in location_data['facilities']:
-        refined_data = {
-            'baseURL': location_data['baseURL'],
-            'daysCount': location_data['daysCount'],
-            'serviceId': location_data['serviceId']
-        }
-        refined_data.update(facility)
-        r = request_data(session, refined_data)
-        facility_labels = {
-            'location': location,
-            'facility': facility['name']
-        }
-        all_matches += getMatches(json.loads(r.content.decode('utf-8')), avail, facility_labels)
-        
-session.close()
+async def main():
+    user_input = get_user_input()
+    duration = user_input["duration"]
+    avail = user_input["availability"]
 
-# sort results by time
-sorted(all_matches, key = lambda x: (x['day'], x['timeStart'], x['location'], x['facility']))
+    # load facility data from file
+    with open('facility_data.json', 'r') as file:
+        facility_data = json.load(file)
 
-# print availability matches
-if (len(all_matches) < 1):
-    print('There are currently no available time slots')
-else:
-    for match in all_matches:
-        print(f'{match['location']} {match['facility']} - {match['day']} {match['timeStart']}-{match['timeEnd']}')
+    all_matches = []
+    tasks = []
+    # iterate through facilities to check their availabilities
+    for location in facility_data:
+        location_data = facility_data[location]
+        for facility in location_data['facilities']:
+            tasks.append(get_facility_matches(location, location_data, facility, duration, avail))
+    all_matches = await asyncio.gather(*tasks)
+    # have to flatten results: list of lists -> list
+    flattened_matches = [item for sublist in all_matches for item in sublist]
+
+    # sort results by time
+    sorted(flattened_matches, key = lambda x: (x['day'], x['timeStart'], x['location'], x['facility']))
+
+    # print availability matches
+    if (len(flattened_matches) < 1):
+        print('There are currently no available time slots')
+    else:
+        for match in flattened_matches:
+            print(f'{match['location']} {match['facility']} - {match['day']} {match['timeStart']}-{match['timeEnd']}')
+
+if __name__ == "__main__":
+    asyncio.run(main())
